3fe2ad3b5ffcb0c84916aa177b8925fb31220c13
Fatih Çetin <fatihcetin1042@gmail.com>
2022-08-16 21:32:46 +0300

    5.C

--- src/Day01Git.java
+++ src/Day01Git.java
@@ -3,6 +3,8 @@ public class Day01Git {
         System.out.println("Hello Git 1. Deneme");
         System.out.println("Hello Git 2. Deneme");
         System.out.println("Hello Git 3. Deneme");
-        System.out.println("Hello Git 4. Deneme");
+
+
+
     }
 }
